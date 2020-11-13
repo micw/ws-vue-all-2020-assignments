e180325cfa317f8fead539a1cfd60adbb13ab040
Michael Wyraz <michael@wyraz.de>
2020-11-13 11:13:27 +0100

    Add comment post

--- ex-2/app.js
+++ ex-2/app.js
@@ -2,7 +2,14 @@ const app = Vue.createApp({
   data() {
     return {
       likesNumber: 0,
-      donations: 0
+      donations: 0,
+      comments: [ {
+        text: "Do commanded an shameless we disposing do. Indulgence ten remarkably nor are impression out. Power is lived means oh every in we quiet. Remainder provision an in intention. Saw supported too joy promotion engrossed propriety. Me till like it sure no sons.",
+        author: "Donald"
+        }],
+      // vers to hold the form. FIXME: do we need global vars here?
+      formText: "",
+      formAuthor: "",
     }
   },
   methods: {
@@ -11,7 +18,13 @@ const app = Vue.createApp({
     },
     donateFiveEuro(num) {
       this.donations += num
-    }
+    },
+    postComment() {
+      // add comment to list of comments
+      this.comments.push({text:this.formText,author:this.formAuthor});
+      // clear the form
+      this.formText=this.formAuthor="";
+    },
   }
 })
 
