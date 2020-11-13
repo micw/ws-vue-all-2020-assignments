a55e3e53dbd5d764e60054bd8b737a675d1f7c3f
Michael Wyraz <michael@wyraz.de>
2020-11-13 10:16:20 +0100

    Add like and donate functionality

--- ex-1/app.js
+++ ex-1/app.js
@@ -1,11 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-
+      numberOfLikes: 0,
+      sumOfDonations: 0
     }
   },
   methods: {
-
+    doLike: function() {
+      this.numberOfLikes++;
+    },
+    doDonate: function() {
+      this.sumOfDonations+=5;
+    }
   }
 })
 
